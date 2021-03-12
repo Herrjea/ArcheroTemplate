2972f4a2fd6edc4e369351f0535dc7b77be1c893
Herrjea <mherreraojea@gmail.com>
2021-03-12 13:43:27 +0100

    New input system (unfinished)

--- ArcheroTemplate/Assets/Scripts/Player/PlayerInput.cs
+++ ArcheroTemplate/Assets/Scripts/Player/PlayerInput.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     PlayerMovementController movementController;
+    Input input;
 
     Touch touch;
 
@@ -12,18 +14,40 @@ public class PlayerInput : MonoBehaviour
     void Start()
     {
         movementController = GetComponent<PlayerMovementController>();
+
+        input = new Input();
+        input.Enable();
+
+        input.Player.Movement.performed += context => DirectMovementInput(context.ReadValue<Vector2>());
+
+        input.Player.Touch.performed += context =>
+        {
+            //if (context.phase == InputActionPhase.Started)
+                Clicked();
+        };
     }
 
 
     void Update()
     {
-        if (Input.touchCount > 0)
+        /*if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
 
             movementController.NewInput(touch.position);
         }
 
-        print(Input.touchCount);
+        print(Input.touchCount);*/
+    }
+
+
+    void DirectMovementInput(Vector2 direction)
+    {
+        print("Key movement towards " + direction);
+    }
+
+    void Clicked()
+    {
+        print("Clicked at " + input.Player.MousePosition.ReadValue<Vector2>());
     }
 }
