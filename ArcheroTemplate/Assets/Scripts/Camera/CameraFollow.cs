f20f833918e22f6cc828ec02664ea0ff3efa6ee9
Herrjea <mherreraojea@gmail.com>
2021-03-15 17:16:45 +0100

    CameraFollow added

--- ArcheroTemplate/Assets/Scripts/Camera/CameraFollow.cs
+++ ArcheroTemplate/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform target;
+
+    [SerializeField] Vector3 offset = new Vector3(0, 20, 0);
+    [SerializeField] float speedSmooth = .08f;
+
+    Vector3 ownPositionFlattened;
+
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        transform.position = target.position + offset;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        ownPositionFlattened = transform.position;
+        ownPositionFlattened.y = target.position.y;
+
+        transform.position =
+            Vector3.Lerp(
+                ownPositionFlattened,
+                target.position,
+                speedSmooth
+            );
+        transform.position += offset;
     }
 }
